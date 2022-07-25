8f36b21d47175e7eea76462926bd45ed426a4605
KevinFMO <fernandomatamoros963@gmail.com>
2022-07-25 14:59:19 -0600

    25/07/2022
    25/07/2022

---
+++ AplicacionBlazor/Blazor/Controllers/LoginController.cs
@@ -0,0 +1,81 @@
+﻿using Blazor.Data;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+using Modelos;
+using System.Security.Claims;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Blazor.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly MySQLConfiguracion _configuracion;
+        private ILoginRepositorio _loginRepositorio;
+        private IUsuarioRepositorio _usuarioRepositorio;
+
+        public LoginController(MySQLConfiguracion configuracion)
+        {
+            _configuracion = configuracion;
+            _loginRepositorio = new LoginRepositorio(configuracion.CadenaConexion);
+            _usuarioRepositorio = new UsuarioRepositorio(configuracion.CadenaConexion);
+        }
+
+        [HttpPost("/account/login")]
+        public async Task<IActionResult> Login(Login login)
+        {
+            string rol = string.Empty;
+            try
+            { 
+                bool usuarioValido = await _loginRepositorio.ValidarUsuarioAsync(login);
+                if (usuarioValido)
+                {
+                    Usuario usu = await _usuarioRepositorio.GetPorCodigo(login.Codigo);
+                    if (usu.EstaActivo)
+                    {
+                        rol = usu.Rol;
+
+                        //Añadimos los claims del usuario y su rol
+                        var claims = new[]
+                        {
+                            new Claim(ClaimTypes.Name, usu.Codigo), 
+                            new Claim(ClaimTypes.Role, rol)
+
+                        };
+
+                        //Craer el claim principal
+                        ClaimsIdentity   clainsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                        ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(clainsIdentity);
+
+                        //Generar la cookie
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal,
+                                                new AuthenticationProperties { IsPersistent = true, ExpiresUtc = DateTime.UtcNow.AddHours(1)});
+                    }
+                    else
+                    {
+                        return LocalRedirect("/login/El Usuario esta inactivo");
+                    }
+
+                }
+                else
+                {
+                    return LocalRedirect("/login/Datos de Usuario invalidos");
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+            return LocalRedirect("/");
+        }
+
+        [HttpGet("/account/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect("/login");
+        }
+
+    }
+}
---
+++ AplicacionBlazor/Blazor/Servicios/UsuarioServicio.cs
@@ -0,0 +1,33 @@
+﻿using Blazor.Interfaces;
+using Modelos;
+
+namespace Blazor.Servicios
+{
+    public class UsuarioServicio : IUsuarioServicio
+    {
+        public Task<bool> Actualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Usuario>> GetLista(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> GetPorCodigo(string codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Nuevo(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
